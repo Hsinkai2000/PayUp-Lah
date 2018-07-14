74ced2f99b7d6a380ee4bd587dabede9d4c5f063
Hsiao Jiet Ng <s10177814@connect.np.edu.sg>
2018-07-14 15:46:35 +0800

    Half way of recyclerview, but dont know how to retrieve data from database

---
+++ app/src/main/java/com/app/teampayup/payuplah/ExpensePlanner.java
@@ -0,0 +1,13 @@
+package com.app.teampayup.payuplah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpensePlanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_planner);
+    }
+}
